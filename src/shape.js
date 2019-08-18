c2d99d23260af7af14118f87807275620cb9edfd
Animuj <michax@gmail.com>
2019-08-18 15:50:12 +0200

    Refactor.

--- src/shape.js
+++ src/shape.js
@@ -1,11 +1,23 @@
 class Shape {
-    super(){
-        console.log('Shape setted up');
+  super() {
+    console.log("Shape setted up");
+  }
+
+  draw() {
+    console.log("Draw shape");
+    let canvas = document.getElementById("game");
+    if (!canvas.getContext) {
+      return;
     }
 
-    draw() {
-        console.log('Draw shape');
-    }
+    let ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "rgb(200, 0, 0)";
+    ctx.fillRect(10, 10, 50, 50);
+
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect(30, 30, 50, 50);
+  }
 }
 
 export default Shape;
