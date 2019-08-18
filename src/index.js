8f61f36384f48abae33e81d100a9644ebfb20794
Animuj <michax@gmail.com>
2019-08-18 16:15:50 +0200

    More commands

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import Shape from './shape';
 
 const shape = new Shape();
-shape.draw();
+shape.drawGrid();
--- src/shape.js
+++ src/shape.js
@@ -1,20 +1,14 @@
 class Shape {
-    super() {
-        let canvas = document.getElementById('game');
-
-        if (!canvas.getContext) {
-            return;
-        }
-
-        this.ctx = canvas.getContext('2d');
+    constructor() {
+        this.ctx = document.getElementById('game').getContext('2d');
     }
 
-    drawGrid(ctx) {
-        ctx.fillStyle = 'rgb(200, 0, 0)';
-        ctx.fillRect(10, 10, 50, 50);
+    drawGrid() {
+        this.ctx.fillStyle = 'rgb(200, 150, 0)';
+        this.ctx.fillRect(10, 10, 50, 50);
 
-        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-        ctx.fillRect(30, 30, 50, 50);
+        this.ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+        this.ctx.fillRect(30, 30, 50, 50);
     }
 }
 
